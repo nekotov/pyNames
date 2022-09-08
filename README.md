85a5972937e467c80430962ea8c2980c01883cd3
Dembytskyi Pavlo <nekto.nekotov@gmail.com>
2022-09-08 21:26:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# pyNames
+[![Build Status](https://travis-ci.org/nekotov/pyNames.svg?branch=master)](https://travis-ci.org/nekotov/pyNames)
+[![Coverage Status](https://coveralls.io/repos/github/nekotov/pyNames/badge.svg?branch=master)](https://coveralls.io/github/nekotov/pyNames?branch=master)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![BCH compliance](https://bettercodehub.com/edge/badge/nekotov/pyNames?branch=master)](https://bettercodehub.com/)
+
+just a simple python minecraft names parser
+## Getting Started
+
+### Prerequisites
+
+python 3.6+ and pip
+
+
+### Installing
+
+```
+git clone git@github.com:nekotov/pyNames.git
+python3 -m pip install -r requirements.txt
+```
+
+### Running
+```
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,4 +1,31 @@
+#!/usr/bin/python3
+
+#  MIT License
+#
+#  Copyright (c) 2022 Dembytskyi Pavlo
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+import threading
 import requests
+
+
 def selections(seq, n):
     if n == 0:
         yield []
@@ -9,13 +36,26 @@ def selections(seq, n):
 
 
 iterable = '0123456789_abcdefghijklmnopqrstuvwxyz'
-sample_size = len(iterable)
-sel = list(selections(iterable, 3))
 
-for item in sel:
-    s = "".join(item)
-    response = requests.get("https://api.mojang.com/users/profiles/minecraft/{0}".format(s))
-    if response.status_code == 204:
-        print("[INFO] {0} is not taken.".format(s))
-    else:
-        print("[INFO] {0} is taken.".format(s))
+
+def run(first_char):
+    print('Starting thread for {}'.format(first_char))
+    sel = list(selections(iterable, 2))
+
+    for item in sel:
+        s = first_char+"".join(item)
+        response = requests.get("https://api.mojang.com/users/profiles/minecraft/{0}".format(s))
+        if response.status_code == 204:
+            print("[INFO] {0} is not taken.".format(s))
+        elif response.status_code == 404:
+            return -1
+
+
+threads = []
+if __name__ == "__main__":
+    for i in range(len(iterable)):
+        t = threading.Thread(target=run, args=(iterable[i],))
+        threads.append(t)
+        t.start()
+
+    threads[-1].join()
